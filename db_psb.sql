0dbb634333e11e6006e74afaff35b0c41750cb8a
Faisal Efendi <faisal19.fe@gmail.com>
2018-07-29 17:53:09 +0800

    v1.03

--- db_psb.sql
+++ db_psb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 26, 2018 at 06:28 AM
+-- Generation Time: Jul 29, 2018 at 11:51 AM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 5.6.33
 
@@ -21,29 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_psb`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbmaster_agama`
---
-
-CREATE TABLE `tbmaster_agama` (
-  `agama_id` int(11) NOT NULL,
-  `agama_nama` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tbmaster_agama`
---
-
-INSERT INTO `tbmaster_agama` (`agama_id`, `agama_nama`) VALUES
-(1, 'Budha'),
-(2, 'Hindu'),
-(3, 'Islam'),
-(4, 'Katolik'),
-(5, 'Kong Hu Cu'),
-(6, 'Protestan');
 
 -- --------------------------------------------------------
 
@@ -121,6 +98,8 @@ CREATE TABLE `tb_pendaftar` (
   `dftr_tgl_lahir` date NOT NULL,
   `dftr_jenkel` char(1) NOT NULL,
   `dftr_alamat` text NOT NULL,
+  `dftr_status` char(1) DEFAULT NULL COMMENT 'null = on proses; 0 = tidak lulus; 1 = lulus;',
+  `dftr_ulang` char(1) DEFAULT NULL COMMENT 'null = blm; 1 = sudah;',
   `dftr_create_by` varchar(3) NOT NULL,
   `dftr_create_dt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -128,12 +107,6 @@ CREATE TABLE `tb_pendaftar` (
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `tbmaster_agama`
---
-ALTER TABLE `tbmaster_agama`
-  ADD PRIMARY KEY (`agama_id`);
 
 --
 -- Indexes for table `tbmaster_orang_tua`
@@ -165,16 +138,10 @@ ALTER TABLE `tb_pendaftar`
 --
 
 --
--- AUTO_INCREMENT for table `tbmaster_agama`
---
-ALTER TABLE `tbmaster_agama`
-  MODIFY `agama_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
 -- AUTO_INCREMENT for table `tbmaster_orang_tua`
 --
 ALTER TABLE `tbmaster_orang_tua`
-  MODIFY `ortu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ortu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
